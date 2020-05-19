efb7cfda627fa27088906a06d220b994912571d9
Eric Tillberg <eric.tillberg@gmail.com>
2020-05-19 08:38:29 -0400

    Make it an object. Is it better? Who knows?

--- imperial.js
+++ imperial.js
@@ -1,49 +1,49 @@
-let gameState = require('./startingGameState');
+const imperial = {
+  play(gameState) {
+    if (this.selectAction(0, gameState) === 0) {
+      return this.buildFactory(gameState)
+    }
+  },
 
-function imperial() {
-  if (selectAction(0, gameState) === 0) {
-    return buildFactory(gameState)
+  selectAction(actionIndex, gameState) {
+    gameState.rondel[actionIndex].push(this.getAustrianPlayer(gameState.investments))
+    return actionIndex
+  },
+  
+  buildFactory(gameState) {
+    return this.payToBank(5, gameState)
+  },
+  
+  getAustrianPlayer(investments) {
+    return this.leadingAustrianInvestment(investments).investor
+  },
+  
+  payToBank(amount, gameState) {
+    gameState.money[gameState.currentCountry] -= amount
+    return amount
+  },
+  
+  leadingAustrianInvestment(investments) {
+    return investments.filter(this.isLeadingAustrianInvestment)[0]
+  },
+  
+  isLeadingAustrianInvestment(investments) {
+    return function(investment) {
+      return investment.amount === Math.max(this.allAustrianInvestments(investments))
+    }
+  },
+  
+  allAustrianInvestments(investments) {
+    return investments.filter(isAustrianInvestment).map(investmentAmount)
+  },
+  
+  isAustrianInvestment(investment) {
+    return investment.country === "austriaHungary";
+  },
+  
+  investmentAmount(investment) {
+    return investment.amount;
   }
-}
-
-function selectAction(actionIndex, gameState) {
-  gameState.rondel[actionIndex].push(getAustrianPlayer(gameState.investments))
-  return actionIndex
-}
-
-function buildFactory(gameState) {
-  return payToBank(5, gameState)
-}
-
-function getAustrianPlayer(investments) {
-  return leadingAustrianInvestment(investments).investor
-}
-
-function payToBank(amount, gameState) {
-  gameState.money[gameState.currentCountry] -= amount
-  return gameState
-}
-
-function leadingAustrianInvestment(investments) {
-  return investments.filter(isLeadingAustrianInvestment)[0]
-}
-
-function isLeadingAustrianInvestment(investments) {
-  return function(investment) {
-    return investment.amount === Math.max(allAustrianInvestments(investments))
-  }
-}
-
-function allAustrianInvestments(investments) {
-  return investments.filter(isAustrianInvestment).map(investmentAmount)
-}
-
-function isAustrianInvestment(investment) {
-  return investment.country === "austriaHungary";
-}
-
-function investmentAmount(investment) {
-  return investment.amount;
 }
 
 module.exports = imperial;
--- imperial.test.js
+++ imperial.test.js
@@ -1,9 +1,16 @@
-const imperial = require('./imperial');
+const imperial = require("./imperial");
+let gameState = require('./startingGameState');
 
-test('puts Austria-Hungary on "production" in the rondel', () => {
-  expect(imperial().rondel[0][0]).toEqual("franzJosef");
-});
+describe("Austria-Hungary selects their first turn action", () => {
+  test("franzJosef is on the first rondel space", () => {
+    expect(imperial.selectAction(0, gameState)).toEqual(0);
+    expect(gameState.rondel[0][0]).toEqual("franzJosef");
+  });
+})
 
-test('buildFactory costs 5 money', () => {
-  expect(imperial().money.austriaHungary).toEqual(-8);
+describe("buildFactory action", () => {
+  test("it costs 5 money", () => {
+    expect(imperial.buildFactory(gameState)).toEqual(5);
+    expect(gameState.money.austriaHungary).toEqual(-3);
+  })
 })
