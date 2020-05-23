560a1b875616be8d0408eed1e341f357a23eab84
Eric Tillberg <eric.tillberg@gmail.com>
2020-05-22 22:02:00 -0400

    Some refactoring and progress

--- imperial.js
+++ imperial.js
@@ -1,79 +1,72 @@
 const imperial = {
-  play(gameState) {
-    if (this.selectAction(0, gameState).rondelPositions.franzJosef === 0) {
-      return this.buildFactory(gameState)
+  getAvailableActions(log) {
+    const lastMove = log[log.length - 1];
+    if (this.shouldReturnRondelActions(lastMove)) {
+      return this.rondelActions(this.getNation(log));
+    } else {
+      return new Set(
+        ["trieste", "prague", "lemburg"].map((province) => ({
+          type: "buildFactory",
+          payload: { province },
+        }))
+      );
     }
   },
 
-  selectAction(actionIndex, gameState) {
-    const austrianPlayer = this.getAustrianPlayer(gameState.investments)
-    return this.updateRondelPositions(gameState, austrianPlayer, actionIndex)
-  },
-  
-  buildFactory(gameState) {
-    return this.payToBank(5, gameState)
-  },
-  
-  getAustrianPlayer(investments) {
-    return this.leadingAustrianInvestment(investments).investor
-  },
-  
-  payToBank(amount, gameState) {
-    const currentCountry = gameState.currentCountry
-    const newAmount = gameState.money[currentCountry] - amount
-    return this.updateMoney(gameState, currentCountry, newAmount)
-  },
-  
-  leadingAustrianInvestment(investments) {
-    return investments.filter(this.isLeadingAustrianInvestment)[0]
-  },
-  
-  isLeadingAustrianInvestment(investments) {
-    return investment => investment.amount === Math.max(this.allAustrianInvestments(investments))
-  },
-  
-  allAustrianInvestments(investments) {
-    return investments.filter(isAustrianInvestment).map(investmentAmount)
-  },
-  
-  isAustrianInvestment(investment) {
-    return investment.country === "austriaHungary";
-  },
-  
-  investmentAmount(investment) {
-    return investment.amount;
+  shouldReturnRondelActions(lastMove) {
+    return (
+      this.logIsEmpty(lastMove) ||
+      this.lastMoveWasBuildFactory(lastMove) ||
+      this.lastMoveWasProduction(lastMove)
+    );
   },
 
-  update(object, key, value) {
-    return Object.assign({}, object, {[key]: value})
+  getNation(log) {
+    const nations = ["AH", "IT", "FR", "GB", "GE", "RU"];
+    const rondelActions = log.filter((action) => action.type === "rondel");
+    if (rondelActions.length > 0) {
+      const lastTurnNation =
+        rondelActions[rondelActions.length - 1].payload.nation;
+      return nations[nations.indexOf(lastTurnNation) + 1];
+    } else {
+      return "AH";
+    }
   },
 
-  updateRondelPositions(gameState, key, value) {
-    const newRondelPositions = this.update(gameState.rondelPositions, key, value)
-    return this.update(gameState, "rondelPositions", newRondelPositions)
+  rondelActions(nation) {
+    return new Set(
+      [
+        "factory",
+        "production1",
+        "maneuver1",
+        "investor",
+        "import",
+        "production2",
+        "maneuver2",
+        "taxation",
+      ].map((slot) => ({
+        type: "rondel",
+        payload: { nation, cost: 0, slot },
+      }))
+    );
   },
 
-  updateMoney(gameState, key, value) {
-    const newMoney = this.update(gameState.money, key, value)
-    return this.update(gameState, "money", newMoney)
-  }
-}
+  logIsEmpty(lastMove) {
+    return !lastMove;
+  },
 
-module.exports = imperial
-module.exports.getAvailableActions = (log) => {
-  if (log.length == 0 || log[log.length - 1].type == 'buildFactory') {
-    // find the last 'rondel' nation
-    // boop up by one on the list of nations
-    return new Set(['factory', 'production1', 'maneuver1', 'investor', 'import', 'production2', 'maneuver2', 
-    'taxation'].map((slot) => ({
-      type: 'rondel', payload: {nation: 'AH', cost: 0, slot}
-    })))
-  } else {
-    return new Set([
-      'trieste', 'prague', 'lemburg'
-    ].map((province) => ({type: 'buildFactory', payload: {province}})))
-  }
-}
+  lastMoveWasBuildFactory(lastMove) {
+    return lastMove.type === "buildFactory";
+  },
+
+  lastMoveWasProduction(lastMove) {
+    return (
+      lastMove.type === "rondel" && lastMove.payload.slot === "production1"
+    );
+  },
+};
+
+module.exports = imperial;
 
 /*
 events = []
--- imperial.test.js
+++ imperial.test.js
@@ -1,48 +1,105 @@
 const imperial = require("./imperial");
-const gameState = require('./startingGameState');
-
-describe("Austria-Hungary selects their first turn action", () => {
-  test("franzJosef is on the first rondel space", () => {
-    expect(imperial.selectAction(0, gameState).rondelPositions.franzJosef).toEqual(0);
-  });
-})
-
-describe("buildFactory action", () => {
-  test("it costs 5 money", () => {
-    expect(imperial.buildFactory(gameState).money.austriaHungary).toEqual(-3);
-  })
-})
 
 describe("available actions", () => {
   test("empty state", () => {
-    const log = []
-    const actions = imperial.getAvailableActions(log)
-    const expected = ['factory', 'production1', 'maneuver1', 'investor', 'import', 'production2', 'maneuver2', 
-  'taxation'].map((slot) => ({
-    type: 'rondel', payload: {nation: 'AH', cost: 0, slot}
-  }))
-    expect(actions).toEqual(new Set(expected))
-  })
+    const log = [];
+    const actions = imperial.getAvailableActions(log);
+    const expected = [
+      "factory",
+      "production1",
+      "maneuver1",
+      "investor",
+      "import",
+      "production2",
+      "maneuver2",
+      "taxation",
+    ].map((slot) => ({
+      type: "rondel",
+      payload: { nation: "AH", cost: 0, slot },
+    }));
+    expect(actions).toEqual(new Set(expected));
+  });
 
   test("AH moved to the factory slot", () => {
-    const log = [{type: 'rondel', payload: {nation: 'AH', cost: 0, slot: "factory"}}]
-    const actions = imperial.getAvailableActions(log)
-    const expected = new Set([
-      'trieste', 'prague', 'lemburg'
-    ].map((province) => ({type: 'buildFactory', payload: {province}})))
-    expect(actions).toEqual(expected) 
-  })
+    const log = [
+      { type: "rondel", payload: { nation: "AH", cost: 0, slot: "factory" } },
+    ];
+    const actions = imperial.getAvailableActions(log);
+    const expected = new Set(
+      ["trieste", "prague", "lemburg"].map((province) => ({
+        type: "buildFactory",
+        payload: { province },
+      }))
+    );
+    expect(actions).toEqual(expected);
+  });
 
   test("AH built a factory in Trieste", () => {
     const log = [
-      {type: 'rondel', payload: {nation: 'AH', cost: 0, slot: "factory"}},
-      {type: 'buildFactory', payload: {province: 'trieste'}}
-    ]
-    const actions = imperial.getAvailableActions(log)
-    const expected = ['factory', 'production1', 'maneuver1', 'investor', 'import', 'production2', 'maneuver2', 
-    'taxation'].map((slot) => ({
-      type: 'rondel', payload: {nation: 'IT', cost: 0, slot}
-    }))
-      expect(actions).toEqual(new Set(expected))
-  })
-})
+      { type: "rondel", payload: { nation: "AH", cost: 0, slot: "factory" } },
+      { type: "buildFactory", payload: { province: "trieste" } },
+    ];
+    const actions = imperial.getAvailableActions(log);
+    const expected = [
+      "factory",
+      "production1",
+      "maneuver1",
+      "investor",
+      "import",
+      "production2",
+      "maneuver2",
+      "taxation",
+    ].map((slot) => ({
+      type: "rondel",
+      payload: { nation: "IT", cost: 0, slot },
+    }));
+    expect(actions).toEqual(new Set(expected));
+  });
+
+  test("IT moved to the production1 slot", () => {
+    const log = [
+      { type: "rondel", payload: { nation: "AH", cost: 0, slot: "factory" } },
+      { type: "buildFactory", payload: { province: "trieste" } },
+      {
+        type: "rondel",
+        payload: { nation: "IT", cost: 0, slot: "production1" },
+      },
+    ];
+    const actions = imperial.getAvailableActions(log);
+    const expected = [
+      "factory",
+      "production1",
+      "maneuver1",
+      "investor",
+      "import",
+      "production2",
+      "maneuver2",
+      "taxation",
+    ].map((slot) => ({
+      type: "rondel",
+      payload: { nation: "FR", cost: 0, slot },
+    }));
+    expect(actions).toEqual(new Set(expected));
+  });
+
+  xtest("FR moved to the maneuver1 slot", () => {
+    const log = [
+      { type: "rondel", payload: { nation: "AH", cost: 0, slot: "factory" } },
+      { type: "buildFactory", payload: { province: "trieste" } },
+      {
+        type: "rondel",
+        payload: { nation: "IT", cost: 0, slot: "production1" },
+      },
+      {
+        type: "rondel",
+        payload: { nation: "FR", cost: 0, slot: "maneuver1" },
+      },
+    ];
+    const actions = imperial.getAvailableActions(log);
+    const expected = [].map((slot) => ({
+      type: "rondel",
+      payload: { nation: "GB", cost: 0, slot },
+    }));
+    expect(actions).toEqual(new Set(expected));
+  });
+});
