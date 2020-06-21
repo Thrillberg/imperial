e03f8ec7cb0dc17954b3bc806e9b439de55009d6
jreut <3959624+jreut@users.noreply.github.com>
2020-06-21 17:33:14 -0400

    Add a memoize() function
    This implementation has some great bugs ~cough~ _features_ for us:
    
    - unlimited storage of arguments
    
    - doesn't do deep equality comparisons of objects

---
+++ memo.js
@@ -0,0 +1,30 @@
+NO_VALUE = Symbol("NO_VALUE");
+
+class Trie {
+  constructor(value) {
+    this.value = value;
+    this.children = new Map();
+  }
+
+  getOrInsert(path, fn, args) {
+    if (path.length === 0) {
+      if (this.value === NO_VALUE) {
+        this.value = fn(...args);
+      }
+      return this.value;
+    }
+    const [head, ...tail] = path;
+    if (!this.children.has(head)) {
+      this.children.set(head, new Trie(NO_VALUE));
+    }
+    return this.children.get(head).getOrInsert(tail, fn, args);
+  }
+}
+
+const memoize = (fn) => {
+  const memory = new Trie(NO_VALUE);
+
+  return (...args) => memory.getOrInsert(args, fn, args);
+};
+
+module.exports = { memoize };
