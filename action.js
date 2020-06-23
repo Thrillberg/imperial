899d234c44b83120f5fa0feb7e9bd9d5a212b523
jreut <3959624+jreut@users.noreply.github.com>
2020-06-23 13:14:24 -0400

    Make Action.* functions actually memoized

--- action.js
+++ action.js
@@ -1,13 +1,38 @@
 const { memoize } = require("./memo");
 
-const makeAction = (type, payloadKeys) =>
-  memoize((obj) => {
+/* This deserves some explanation.
+ *
+ * Our memoize() function supports only primitive arguments; that
+ * is, it cannot perform deep equality checks on objects. That's a
+ * problem, because we want our action creation functions to accept
+ * a single object as their argument. We hack around that with some
+ * assumptions:
+ *
+ *   - our arguments are objects whose values are all primitives
+ *     (ie. { huhu: 2 } instead of { huhu: { bebe: 2 } }
+ *   - our objects' keys have a stable lexical sorting (ie. they
+ *     are all strings)
+ *
+ * Those assumptions let us flatten the object in a stable way into
+ * memoizable arguments:
+ *
+ *   1. sort the expected argument keys
+ *   2. memoize a function that expects values in the order of those
+ *      sorted keys
+ *   3. wrap the memoized function in a function that translates a
+ *      single, object argument into a list of sorted values
+ */
+const makeAction = (type, payloadKeys) => {
+  payloadKeys.sort();
+  const memoized = memoize((...values) => {
     const payload = {};
-    for (const key of payloadKeys) {
-      payload[key] = obj[key];
-    }
+    values.forEach((v, i) => {
+      payload[payloadKeys[i]] = v;
+    });
     return { type, payload };
   });
+  return (obj) => memoized(...payloadKeys.map((k) => obj[k]));
+};
 
 const specs = {
   bondPurchase: ["nation", "player", "cost"],
