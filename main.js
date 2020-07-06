7c5d62a1fde560c2818a312fc1f9ccb82f55b6be
Eric Tillberg <eric.tillberg@gmail.com>
2020-07-06 09:58:21 -0400

    Display cash correctly

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 import Imperial from "./imperial.js";
 import log from "./schnelleinsteigLog.js";
 
+Vue.component("player", {
+  props: ["name", "cash"],
+  template: "<li>{{ name }} has {{ cash }} million in cash!</li>",
+});
+
 var app = new Vue({
   el: "#app",
   data: {
     game: {},
     gameStarted: false,
-    players: [],
+    gameLog: [],
+    logIndex: 0,
   },
   methods: {
-    startGame: () => {
-      app.game = Imperial.fromLog([]);
-      app.game.tick(log[0]);
-      app.game.order.map((player) => {
-        app.players.push(player);
-      });
-      app.gameStarted = true;
+    startGame: function () {
+      this.gameLog.push(log[0]);
+      this.game = Imperial.fromLog(this.gameLog);
+      this.logIndex += 1;
+      this.gameStarted = true;
     },
-    tick: () => {},
+    tick: function () {
+      this.logIndex += 1;
+      this.gameLog.push(log[this.logIndex]);
+      this.game = Imperial.fromLog(this.gameLog);
+    },
   },
 });
