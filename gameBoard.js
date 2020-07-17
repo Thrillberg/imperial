9deceb1ca4323fe6ca491931cec3c44353989e2c
jreut <3959624+jreut@users.noreply.github.com>
2020-07-17 12:25:58 -0400

    Implement "railroad rule"

--- gameBoard.js
+++ gameBoard.js
@@ -1,16 +1,38 @@
 export default class GameBoard {
   constructor(nodes, edges) {
     this.graph = new Map();
+    this.byNation = new Map();
+
     for (const [province, nation] of nodes) {
-      this.graph.set(province, new Set());
+      this.graph.set(province, {
+        nation,
+        neighbors: new Set(),
+      });
+      if (!this.byNation.has(nation)) {
+        this.byNation.set(nation, new Set());
+      }
+      this.byNation.get(nation).add(province);
     }
     edges.forEach(([a, b]) => {
-      this.graph.get(a).add(b);
-      this.graph.get(b).add(a);
+      this.graph.get(a).neighbors.add(b);
+      this.graph.get(b).neighbors.add(a);
     });
   }
 
-  neighborsFor({ province }) {
-    return this.graph.get(province);
+  neighborsFor({ province, nation }) {
+    if (!this.graph.has(province))
+      throw new Error(`province ${province} not found`);
+    if (!this.byNation.has(nation))
+      throw new Error(`nation ${nation} not found`);
+
+    const out = new Set();
+    for (const p of this.byNation.get(nation)) {
+      for (const n of this.graph.get(p).neighbors) {
+        out.add(n);
+      }
+    }
+    out.delete(province);
+
+    return out;
   }
 }
--- gameBoard.test.js
+++ gameBoard.test.js
@@ -1,7 +1,22 @@
 import GameBoard from "./gameBoard";
 
 describe("GameBoard", () => {
-  test("it works really well", () => {
+  test("empty", () => {
+    const gameBoard = new GameBoard(new Map(), []);
+    expect(() =>
+      gameBoard.neighborsFor({ province: "oops", nation: "big oops" })
+    ).toThrowError();
+  });
+
+  test("unknown nation", () => {
+    const gameBoard = new GameBoard(new Map([["1", "a"]]), []);
+
+    expect(() =>
+      gameBoard.neighborsFor({ province: "1", nation: "oops" })
+    ).toThrowError();
+  });
+
+  test("edges are undirected", () => {
     const gameBoard = new GameBoard(
       new Map([
         ["p1", "n1"],
@@ -10,13 +25,32 @@ describe("GameBoard", () => {
       [["p1", "p2"]]
     );
 
-    const originUnit = {
-      province: "p1",
-      nation: "n1",
-    };
+    expect(gameBoard.neighborsFor({ province: "p1", nation: "n1" })).toEqual(
+      new Set(["p2"])
+    );
+    expect(gameBoard.neighborsFor({ province: "p2", nation: "n1" })).toEqual(
+      new Set(["p1"])
+    );
+  });
 
-    const neighbors = new Set(["p2"]);
+  test("railroad rule", () => {
+    const gameBoard = new GameBoard(
+      new Map([
+        ["1", "a"],
+        ["2", "a"],
+        ["3", "b"],
+      ]),
+      [
+        ["1", "2"],
+        ["2", "3"],
+      ]
+    );
 
-    expect(gameBoard.neighborsFor(originUnit)).toEqual(neighbors);
+    expect(gameBoard.neighborsFor({ province: "1", nation: "a" })).toEqual(
+      new Set(["2", "3"])
+    );
+    expect(gameBoard.neighborsFor({ province: "1", nation: "b" })).toEqual(
+      new Set(["2"])
+    );
   });
 });
