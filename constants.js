7b774c875a107122a5d97d35a496bbe73fbaa126
Eric Tillberg <eric.tillberg@gmail.com>
2020-07-24 14:31:22 -0400

    Add Bond constant

--- constants.js
+++ constants.js
@@ -1,7 +1,23 @@
 import { Enum } from "./enum.js";
+import { memoize } from "./memo.js";
 
 const Nation = Enum.fromArray(["AH", "IT", "FR", "GB", "GE", "RU"], "Nation");
 
-export const Bond = () => ({ theAnswer: 42 });
+const cost = {
+  1: 2,
+  2: 4,
+  3: 6,
+  4: 9,
+  5: 12,
+  6: 16,
+  7: 20,
+  8: 25,
+  9: 30,
+};
+export const Bond = memoize((nation, number) => ({
+  nation,
+  number,
+  cost: cost[number],
+}));
 
 export { Nation };
