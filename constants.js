2729f83657c9d2e8880ca3757eb3373c0cda7f02
jreut <3959624+jreut@users.noreply.github.com>
2020-07-24 14:16:04 -0400

    Add failing Bond tests

--- constants.js
+++ constants.js
@@ -2,4 +2,6 @@ import { Enum } from "./enum.js";
 
 const Nation = Enum.fromArray(["AH", "IT", "FR", "GB", "GE", "RU"], "Nation");
 
+export const Bond = () => ({ theAnswer: 42 });
+
 export { Nation };
