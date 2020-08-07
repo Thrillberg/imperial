cd6f47518532a8f7bdee90e8b3c6817b5e2ee181
jreut <3959624+jreut@users.noreply.github.com>
2020-08-07 18:36:52 -0400

    Teach memoize() simplisitc deep equality
    This one deserves a whole lot of explanation.

--- memo.js
+++ memo.js
@@ -1,5 +1,11 @@
 const NO_VALUE = Symbol("NO_VALUE");
 
+const SET_START = Symbol("SET_START");
+const ARY_START = Symbol("ARY_START");
+const MAP_START = Symbol("MAP_START");
+const OBJ_START = Symbol("OBJ_START");
+const ITER_END = Symbol("ITER_END");
+
 class Trie {
   constructor(value) {
     this.value = value;
@@ -14,6 +20,38 @@ class Trie {
       return this.value;
     }
     const [head, ...tail] = path;
+    if (head instanceof Array) {
+      return this.getOrInsert(
+        [ARY_START, ...head, ITER_END, ...tail],
+        fn,
+        args
+      );
+    }
+    if (head instanceof Set) {
+      const entries = [...head];
+      entries.sort();
+      return this.getOrInsert(
+        [SET_START, ...entries, ITER_END, ...tail],
+        fn,
+        args
+      );
+    }
+    if (head instanceof Map) {
+      const entries = [...head.entries()].sort();
+      return this.getOrInsert(
+        [MAP_START, ...entries, ITER_END, ...tail],
+        fn,
+        args
+      );
+    }
+    if (head instanceof Object) {
+      const entries = Object.entries(head).sort();
+      return this.getOrInsert(
+        [OBJ_START, ...entries, ITER_END, ...tail],
+        fn,
+        args
+      );
+    }
     if (!this.children.has(head)) {
       this.children.set(head, new Trie(NO_VALUE));
     }
--- memo.test.js
+++ memo.test.js
@@ -33,10 +33,7 @@ describe("memoize()", () => {
     expect(callCount).toBe(1);
   });
 
-  /* This may seem like a bug instead of a feature.
-   * This may be a correct assumption.
-   */
-  test("deep equality does not work", () => {
+  test("arbitrary objects", () => {
     let callCount = 0;
 
     const fn = memoize((arg1, arg2) => {
@@ -44,11 +41,105 @@ describe("memoize()", () => {
       return [arg1 + 1, arg2.huhu + 2];
     });
 
-    /* n.b. we create the object twice to get a different object id */
     expect(callCount).toBe(0);
-    expect(fn(42, { huhu: 108 })).toEqual([43, 110]);
+    const out = fn(42, { huhu: 108 });
+    expect(out).toEqual([43, 110]);
     expect(callCount).toBe(1);
-    expect(fn(42, { huhu: 108 })).toEqual([43, 110]);
-    expect(callCount).toBe(2);
+    expect(fn(42, { huhu: 108 })).toBe(out);
+    expect(callCount).toBe(1);
+  });
+
+  test("arrays", () => {
+    let callCount = 0;
+
+    const fn = memoize((ary) => {
+      callCount++;
+      return [...ary, 42];
+    });
+
+    expect(callCount).toBe(0);
+    const out = fn([1, 2]);
+    expect(callCount).toBe(1);
+    expect(out).toEqual([1, 2, 42]);
+    expect(fn([1, 2])).toBe(out);
+    expect(callCount).toBe(1);
+  });
+
+  test("sets", () => {
+    let callCount = 0;
+
+    const fn = memoize((s) => {
+      callCount++;
+      return new Set([...s, 42]);
+    });
+
+    expect(callCount).toBe(0);
+    const out = fn(new Set([1, 2]));
+    expect(callCount).toBe(1);
+    expect(out).toEqual(new Set([1, 2, 42]));
+    expect(fn(new Set([2, 1]))).toBe(out);
+    expect(callCount).toBe(1);
+  });
+
+  test("maps", () => {
+    let callCount = 0;
+
+    const fn = memoize((s) => {
+      callCount++;
+      return new Map([...s.entries(), ["the answer", 42]]);
+    });
+
+    expect(callCount).toBe(0);
+    const out = fn(new Map([["the question", 2]]));
+    expect(callCount).toBe(1);
+    expect(out).toEqual(
+      new Map([
+        ["the question", 2],
+        ["the answer", 42],
+      ])
+    );
+    expect(fn(new Map([["the question", 2]]))).toBe(out);
+    expect(callCount).toBe(1);
+  });
+
+  test("deep collections", () => {
+    let callCount = 0;
+
+    const input1 = new Map([
+      ["evens", [true, new Set([2, 4, 6])]],
+      ["odds", [false, new Set([1, 3])]],
+    ]);
+
+    const input2 = new Map([
+      ["odds", [false, new Set([3, 1])]],
+      ["evens", [true, new Set([6, 2, 4])]],
+    ]);
+
+    const expected = new Map([
+      ["odds", new Set([3])],
+      ["evens", new Set([4, 6])],
+    ]);
+
+    const fn = memoize((m) => {
+      callCount++;
+      const out = new Map();
+      for (const [k, [_, v]] of m) {
+        const newV = new Set();
+        for (const e of v) {
+          if (e > 2) {
+            newV.add(e);
+          }
+        }
+        out.set(k, newV);
+      }
+      return out;
+    });
+
+    expect(callCount).toBe(0);
+    const result = fn(input1);
+    expect(callCount).toBe(1);
+    expect(result).toEqual(expected);
+    expect(fn(input2)).toBe(result);
+    expect(callCount).toBe(1);
   });
 });
