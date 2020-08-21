49f437cfe50a042fcee0fe811033d853cdd3963f
Eric Tillberg <eric.tillberg@gmail.com>
2020-08-21 14:49:24 -0400

    Add base Vue app

---
+++ imperial-vue/vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'default',
+      overrideEndpoint: false,
+      region: 'us-east-1',
+      bucket: 'playimperial.club',
+      createBucket: true,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: false,
+      pluginVersion: '4.0.0-rc3',
+      uploadConcurrency: 5
+    }
+  }
+}
