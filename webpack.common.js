7373bb4452b583d32b0c69b35a6f6889162bc5a8
Eric Tillberg <eric.tillberg@gmail.com>
2020-08-23 19:41:17 -0400

    Split off dev and prod webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,9 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
-  devServer: { contentBase: "./dist" },
+  entry: {
+    app: "./src/index.js",
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
