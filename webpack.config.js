11d385e8080ba3ccfd59df68234f4e87fb10a869
Eric Tillberg <eric.tillberg@gmail.com>
2020-08-23 15:11:50 -0400

    Swap development server
    Removed static-server and added webpack-dev-server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: { contentBase: "./dist" },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
