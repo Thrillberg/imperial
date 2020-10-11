b8655f4846dc57c05729c6d48904ac0b94ce0fb8
Eric Tillberg <eric.tillberg@gmail.com>
2020-10-11 17:48:45 -0400

    Add tax chart

---
+++ client/src/components/TaxChartSlot.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex justify-evenly items-center h-12">
+    <span class="text-2xl">{{ taxSlot }}</span>
+    <span class="w-24 flex flex-wrap"
+      ><Flag
+        v-for="nation in nations"
+        v-bind:nation="nation"
+        height="15"
+        v-bind:class="'my-1 mx-auto'"
+        v-bind:key="nation"
+      ></Flag
+    ></span>
+    <span class="text-2xl">+{{ taxSlot - 5 }}</span>
+  </div>
+</template>
+
+<script>
+import Flag from "./Flag.vue";
+
+export default {
+  name: "TaxChartSlot",
+  components: {
+    Flag,
+  },
+  props: {
+    taxSlot: Number,
+    nations: Array,
+  },
+};
+</script>
