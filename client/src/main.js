fe7096fa2b911151929f704f239a8af7a4895ad1
Eric Tillberg <eric.tillberg@gmail.com>
2020-10-29 12:12:45 -0400

    Add Vue router

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import Home from "./Home.vue";
+import router from "./router";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(Home)
 }).$mount("#app");
