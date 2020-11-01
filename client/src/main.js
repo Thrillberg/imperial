815d6d67d52bb6015ab6e4beadc06a2d3ebd2b78
Eric Tillberg <eric.tillberg@gmail.com>
2020-11-01 16:01:47 -0500

    Use cookies instead of localStorage

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueCookies from "vue-cookies";
 
 Vue.config.productionTip = false;
+Vue.use(VueCookies);
 
 new Vue({
   router,
