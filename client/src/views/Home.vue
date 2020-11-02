c00c2752704463d0705a85d7566303c6b7f78111
Eric Tillberg <eric.tillberg@gmail.com>
2020-11-01 19:41:57 -0500

    Improve Home view

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,40 +1,60 @@
 <template>
-  <div>
-    Users:
-    <ul v-for="user in users" v-bind:key="user.id">
-      <li v-if="user.name === name">
-        <strong>{{ user.name }}</strong>
-      </li>
-      <li v-else>
-        {{ user.name }}
-      </li>
-    </ul>
-    Games:
-    <ul v-for="game in games" v-bind:key="game.id">
-      <li>
-        <router-link :to="{ path: '/game/' + game.id }">
-          Hosted by {{ game.host }}
-        </router-link>
-        <span v-if="gameStarted(game.id)">Game Started!</span>
-        <router-link
-          :to="{ path: '/game/' + game.id }"
-          v-if="notMyGame(game.id)"
-          v-on:click.native="joinGame(game.id)"
-        >
-          Join Game
-        </router-link>
-      </li>
-    </ul>
-    <div v-if="alreadyRegistered()">
-      <button v-on:click="openGame()">Play</button>
+  <div class="container">
+    <div class="text-center">
+      <div>
+        <div class="underline">Users:</div>
+        <ul v-for="user in users" v-bind:key="user.id">
+          <li v-if="user.name === name">
+            <strong>{{ user.name }}</strong>
+          </li>
+          <li v-else>
+            {{ user.name }}
+          </li>
+        </ul>
+      </div>
+      <div class="mt-4">
+        <div class="underline">Games:</div>
+        <ul v-for="game in games" v-bind:key="game.id">
+          <li class="p-2">
+            <router-link :to="{ path: '/game/' + game.id }">
+              Hosted by {{ game.host }}
+            </router-link>
+            <span
+              v-if="gameStarted(game.id)"
+              class="rounded p-2 m-1 bg-red-600 text-white font-bold"
+              >Game Started!</span
+            >
+            <router-link
+              :to="{ path: '/game/' + game.id }"
+              v-if="joinable(game.id)"
+              v-on:click.native="joinGame(game.id)"
+              class="rounded p-2 m-1 bg-green-400"
+            >
+              Join Game
+            </router-link>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div v-else class="flex flex-col">
+    <div v-if="alreadyRegistered()" class="text-center mt-20">
+      <span
+        v-on:click="openGame()"
+        class="rounded p-4 ml-4 bg-green-800 text-white cursor-pointer"
+        >Play</span
+      >
+    </div>
+    <div v-else class="text-center mt-20">
       <input
-        class="mx-auto m-6 border-black border-solid border-2 p-3 rounded"
+        class="mx-auto m-6 border-black border-solid border p-3 rounded"
         v-model="name"
         placeholder="name"
       />
-      <button v-on:click="registerUser(name)">Register</button>
+      <span
+        v-on:click="registerUser(name)"
+        class="rounded p-4 ml-4 bg-green-800 text-white cursor-pointer"
+      >
+        Register
+      </span>
     </div>
 
     <hr class="mt-40" />
@@ -142,8 +162,10 @@ export default {
         return false;
       }
     },
-    notMyGame: function (gameId) {
-      return this.games.find((game) => game.id === gameId).host !== this.name;
+    joinable: function (gameId) {
+      let notMyGame =
+        this.games.find((game) => game.id === gameId).host !== this.name;
+      return notMyGame && this.alreadyRegistered() && !this.gameStarted(gameId);
     },
     joinGame: function (gameId) {
       this.webSocket.send(
