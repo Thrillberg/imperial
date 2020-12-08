1045ba8d59e93b82459123b560dc032511700a49
Eric Tillberg <eric.tillberg@gmail.com>
2020-12-08 18:34:02 -0500

    Add APIClient

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,96 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 
+class APIClient {
+  constructor() {
+    this.ws = this.initws();
+    this.handlers = {};
+  }
+
+  initws() {
+    const ws = new WebSocket(process.env.VUE_APP_IMPERIAL_WEBSOCKETS_URL);
+    ws.onmessage = message => {
+      const envelope = JSON.parse(message.data);
+      if (this.handlers[envelope.kind]) {
+        this.handlers[envelope.kind](envelope.data);
+      } else {
+        console.error(envelope);
+        throw new Error(`unhandled kind: ${envelope.kind}`);
+      }
+    };
+    ws.onclose = this.onclose.bind(this);
+    return ws;
+  }
+
+  onclose() {
+    console.info("replacing closed websocket");
+    this.ws = this.initws();
+  }
+
+  send(data) {
+    try {
+      this.ws.send(JSON.stringify(data));
+    } catch (e) {
+      console.error("sending message", e);
+    }
+  }
+
+  clearHandlers() {
+    this.handlers = {};
+  }
+
+  onUserRegistered(cb) {
+    if (this.handlers["userRegistered"] !== undefined) {
+      throw new Error("there is already a handler defined");
+    }
+    this.handlers["userRegistered"] = cb;
+  }
+
+  onGameOpened(cb) {
+    if (this.handlers["gameOpened"] !== undefined) {
+      throw new Error("there is already a handler defined");
+    }
+    this.handlers["gameOpened"] = cb;
+  }
+
+  onUpdateGameLog(cb) {
+    if (this.handlers["updateGameLog"] !== undefined) {
+      throw new Error("there is already a handler defined");
+    }
+    this.handlers["updateGameLog"] = cb;
+  }
+
+  joinGame(userId, gameId, userName) {
+    return this.send({
+      kind: "joinGame",
+      data: { userName, userId, gameId }
+    });
+  }
+
+  openGame(host) {
+    return this.send({
+      kind: "registerUser",
+      data: { host }
+    });
+  }
+
+  registerUser(name) {
+    return this.send({
+      kind: "registerUser",
+      data: { name }
+    });
+  }
+
+  tick(gameId, action) {
+    return this.send({
+      kind: "tick",
+      data: { gameId, action }
+    });
+  }
+}
+
+const apiClient = new APIClient();
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -24,3 +114,4 @@ const router = new VueRouter({
 });
 
 export default router;
+export { apiClient };
