0e3db4b14fe92f957d8925c13a9abaab8618f9f2
Eric Tillberg <eric.tillberg@gmail.com>
2021-01-18 20:49:10 -0500

    Set Max-Age on cookie
    Fixes #126

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,9 @@ class SessionsController < ApplicationController
     user = User.create(name: "anonymous")
     cookies[:user_id] = {
       value: user.id,
-      same_site: :lax
+      same_site: :lax,
+      # This cookie will expire in 68 years!
+      max_age: 2147483647
     }
   end
 end
