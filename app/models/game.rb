4d1588238cc6d0ce59079b4acd56838a5e9cd604
Aaron H <armahillo@gmail.com>
2021-01-21 21:22:16 -0500

    Refactor channel logic (#139)

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,14 @@ class Game < ActiveRecord::Base
   has_many :players
   has_many :users, through: :players
   belongs_to :host, class_name: "User"
+
+  def to_json
+    {
+      name: name,
+      id: id,
+      host: host.name,
+      players: users.map(&:name),
+      log: actions.map(&:data)
+    }
+  end
 end
