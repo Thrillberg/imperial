38edf0dd80a38b7c6e752c34d866ff40f502b546
Eric Tillberg <eric.tillberg@gmail.com>
2021-01-21 19:52:06 -0500

    Add prettier (#147)
    Fixes #146

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'prettier',
+    'prettier/vue'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
