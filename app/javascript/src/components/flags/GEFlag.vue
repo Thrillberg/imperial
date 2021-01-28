35b7356ff829f71d1f0647d56712e21db8e837cf
Eric Tillberg <eric.tillberg@gmail.com>
2021-01-28 15:14:01 -0500

    Fix rondel flags on Chrome

--- app/javascript/src/components/flags/GEFlag.vue
+++ app/javascript/src/components/flags/GEFlag.vue
@@ -1,17 +1,18 @@
 <template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 1500 1000"
-    :width="width"
-    :height="height"
-    :transform="transform"
-  >
-    <g v-bind:clip-path="this.fleet ? 'url(#fleetClip)' : ''">
-      <path d="M0 0h1500v1000H0z" />
-      <path d="M0 333h1500v666H0z" fill="#fff" />
-      <path d="M0 666h1500v333H0z" fill="#d00" />
-    </g>
-  </svg>
+  <g :transform="transform">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1500 1000"
+      :width="width"
+      :height="height"
+    >
+      <g v-bind:clip-path="this.fleet ? 'url(#fleetClip)' : ''">
+        <path d="M0 0h1500v1000H0z" />
+        <path d="M0 333h1500v666H0z" fill="#fff" />
+        <path d="M0 666h1500v333H0z" fill="#d00" />
+      </g>
+    </svg>
+  </g>
 </template>
 
 <script>
