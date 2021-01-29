6425d2e36be996e946691edef899c9c6004209af
Eric Tillberg <eric.tillberg@gmail.com>
2021-01-29 11:47:07 -0500

    standardrb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :accounts, controllers: {
     registrations: "accounts/registrations",
     sessions: "accounts/sessions"
-  }, defaults: { format: :json }
+  }, defaults: {format: :json}
   mount ActionCable.server => "/ws"
   get "/health", to: "health#index"
 
