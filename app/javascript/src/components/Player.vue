9c0a307a5499f7b58c71ab30e5bb32210ee8ad54
Eric Tillberg <eric.tillberg@gmail.com>
2021-01-30 09:24:56 -0500

    Center rondel and display player cash (#186)
    Fixes #185

--- app/javascript/src/components/Player.vue
+++ app/javascript/src/components/Player.vue
@@ -1,9 +1,9 @@
 <template>
-  <div
-    class="p-2 m-1 bg-green-200 font-serif"
-    :class="player.name === current_player ? 'font-bold' : ''"
-  >
-    <h3>{{ player.name }}</h3>
+  <div class="p-2 m-1 bg-green-200 font-serif">
+    <span :class="player.name === current_player ? 'font-bold' : ''">
+      {{ player.name }}
+    </span>
+    <span v-if="player.name === current_player"> - {{ player.cash }}m</span>
     <div class="flex flex-wrap">
       <Bond
         v-for="bond in player.bonds"
