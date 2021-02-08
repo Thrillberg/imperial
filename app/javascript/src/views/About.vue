dbea6f39397a04c199fd4aab255a5cc18ced7e2d
Eric Tillberg <eric.tillberg@gmail.com>
2021-02-08 15:26:42 -0500

    Add Slack link to About page

--- app/javascript/src/views/About.vue
+++ app/javascript/src/views/About.vue
@@ -4,5 +4,6 @@
     <p>
       <b>PlayImperial</b> is a web implementation of the board game, <a href="https://en.wikipedia.org/wiki/Imperial_(board_game)" class="underline">Imperial</a> by Mac Gerdts. The board game can be purchased from the publisher <a href="https://www.pd-verlag.de/epages/es719366.sf/en_GB/?ObjectPath=/Shops/es719366/Products/9703" class="underline">here</a>.</p>
     <p>PlayImperial in an open-source project, the code for which can be found <a href="https://www.github.com/thrillberg/imperial" class="underline">on GitHub</a>. We always welcome new contributors!</p>
+    <p>Feel free to say hi <a href="https://join.slack.com/t/playimperial/shared_invite/zt-ljyq6486-L9~~~ksHdqe23hN71y848w" class="underline">on Slack</a>!</p>
   </div>
 </template>
