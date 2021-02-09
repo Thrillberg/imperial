9f8457724b8dd460d799a8c3c69bbc9fdb7f09b9
Eric Tillberg <eric.tillberg@gmail.com>
2021-02-09 13:18:31 -0500

    Fix bug when registering user (#232)
    Fixes #230

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   belongs_to :account, optional: true
 
   validates :name, uniqueness: {case_sensitive: false}
+
+  def convert_games(old_name)
+    games.each do |game|
+      initialize_action = game.actions.order(:created_at).first
+      data = initialize_action.data.gsub(old_name, name)
+      initialize_action.update(data: data)
+    end
+  end
 end
