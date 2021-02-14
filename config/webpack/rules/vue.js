27dbc7a4e6f7bdbb63da99fc413cdfd818744f4d
Eric Tillberg <eric.tillberg@gmail.com>
2021-02-13 19:20:48 -0500

    Add robots.txt

--- config/webpack/rules/vue.js
+++ config/webpack/rules/vue.js
@@ -27,6 +27,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       alwaysWriteToDisk: true,
       favicon: "favicon.ico",
+      meta: {
+        charset: "UTF-8",
+        description: "Play Imperial online",
+        keywords: "imperial, board game, game, online, play",
+        viewport: "width=device-width, initial-scale=1"
+      },
       title: "Imperial"
     }),
     new HtmlWebpackHarddiskPlugin({
