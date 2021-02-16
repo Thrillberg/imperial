26d456eb0004ce8ea7231a88b0a1be20109f7a89
Eric Tillberg <eric.tillberg@gmail.com>
2021-02-15 20:58:31 -0500

    Display all available bonds even if player cannot afford them (#272)
    Fixes #260

--- app/javascript/src/components/Bond.vue
+++ app/javascript/src/components/Bond.vue
@@ -1,10 +1,10 @@
 <template>
-  <div :class="'m-2 border-2 border-' + border() +'-500 p-1 tooltip bg-' + bond.nation.value">
+  <div :class="'m-2 border-2 border-' + border() +'-500 p-1 tooltip bg-' + bond.nation.value" :style="filter === 'grayscale' ? {filter: 'grayscale(1)'} : {}">
     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="20">
       <Flag :nation="bond.nation.value" width="30" />
     </svg>
     {{ bond.number }}:{{ bond.cost }}
-    <div v-if="!!tradedBond" class="tooltip-text border border-gray-500 p-1 rounded mt-3">
+    <div v-if="!!tradedBond" class="tooltip-text border border-red-500 p-1 rounded mt-3 bg-white">
       Trade in for {{ this.tradedBondString() }}.
     </div>
   </div>
@@ -17,14 +17,14 @@ import stringify from "../stringify.js";
 
 export default {
   name: "Bond",
-  props: { bond: Object, tradedBond: Object },
+  props: { bond: Object, filter: String, tradedBond: Object },
   components: { Flag },
   methods: {
     border: function() {
       if (!!this.tradedBond) {
         return "red";
       } else {
-        return "gray";
+        return "green";
       }
     },
     tradedBondString: function() {
