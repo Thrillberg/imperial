d0ec045b7ed4dfada7e28fdd40afd6d1a005ece9
Eric Tillberg <eric.tillberg@gmail.com>
2021-02-24 13:58:50 -0500

    Add feedback form link

--- app/javascript/src/components/Header.vue
+++ app/javascript/src/components/Header.vue
@@ -2,7 +2,7 @@
   <header>
     <div class="bg-green-500 flex justify-between">
       <div class="bg-white m-2 border-2 rounded border-red-500 p-4">
-        This project is under active development and data may be lost at any time!
+        Please fill out our <a href="https://forms.gle/kwqzuQwfJ7QBRipz8">feedback form</a> so we can keep improving the site!
       </div>
       <div class="flex">
         <router-link
