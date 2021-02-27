064633b1831e52edd4329a765be39a7224e64fca
Eric Tillberg <eric.tillberg@gmail.com>
2021-02-27 09:34:45 -0500

    Update abandoned conditions

--- app/models/game.rb
+++ app/models/game.rb
@@ -19,6 +19,8 @@ class Game < ActiveRecord::Base
   def abandoned?
     if actions.length > 0 && !force_ended_at
       return actions.order(:created_at).last.created_at < 3.days.ago
+    elsif !force_ended_at
+      return created_at < 3.days.ago
     end
 
     false
