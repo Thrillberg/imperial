70644b4c072d60a9180c02fa5808f1315456e295
Eric Tillberg <eric.tillberg@gmail.com>
2021-02-28 20:08:55 -0500

    Implement UI changes
    Fixes #342

--- app/javascript/src/views/Home.vue
+++ app/javascript/src/views/Home.vue
@@ -16,12 +16,6 @@
         </button>
       </div>
       <div v-else class="flex justify-around items-start">
-        <button
-          @click="openGame"
-          class="rounded bg-green-800 text-white cursor-pointer block w-1/4 text-2xl hover:bg-green-900 p-10 m-10"
-        >
-          Open a New Game
-        </button>
         <div class="w-3/4" v-if="gamesFetched">
           <YourGames :games="yourGames" :profile="profile"></YourGames>
           <UnstartedGameList :games="unstartedGames" :profile="profile"></UnstartedGameList>
@@ -50,14 +44,17 @@ export default {
       return this.games.filter(game => {
         return(
           game.players.includes(this.profile.username) &&
-          game.log.length > 0 &&
           !game.forceEndedAt
         )
       })
     },
     unstartedGames() {
       let games = this.games.filter(game => {
-        return game.log.length === 0 && !game.forceEndedAt
+        return(
+          game.log.length === 0 &&
+          !game.players.includes(this.profile.username) &&
+          !game.forceEndedAt
+        )
       });
       return games.sort((a, b) => {
         return new Date(b.createdAt) - new Date(a.createdAt);
@@ -65,12 +62,6 @@ export default {
     }
   },
   methods: {
-    openGame() {
-      apiClient.openGame(this.$cookies.get("user_id"))
-        .then(game => {
-          this.$router.push(`/game/${game.id}`);
-        });
-    },
     setAnonymous() {
       fetch(
         "/anonymity_confirmations",
