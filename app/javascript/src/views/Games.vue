c2ea6431691899b4377b00c1b2a81d0422a207ee
Eric Tillberg <eric.tillberg@gmail.com>
2021-03-01 16:14:18 -0500

    Sort all games list

--- app/javascript/src/views/Games.vue
+++ app/javascript/src/views/Games.vue
@@ -5,7 +5,7 @@
       <div class="w-1/2"><b>Name</b></div>
       <div class="w-1/2"><b>Players</b></div>
     </div>
-    <div v-for="game of games" v-bind:key="game.id">
+    <div v-for="game of sortedGames" v-bind:key="game.id">
       <router-link :to="{ path: '/game/' + game.id }" class="flex justify-between items-center hover:bg-gray-200 py-2">
         <div class="w-1/2">{{ game.name }}</div>
         <div class="w-1/2">{{ game.players.join(", ") }}</div>
@@ -17,6 +17,13 @@
 <script>
 export default {
   name: "Games",
-  props: { games: Array, profile: Object }
+  props: { games: Array, profile: Object },
+  computed: {
+    sortedGames() {
+      return this.games.sort((a, b) => {
+        return new Date(b.createdAt) - new Date(a.createdAt);
+      });
+    }
+  }
 }
 </script>
