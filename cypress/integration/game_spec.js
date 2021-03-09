ea6de03a9d2aa334f8aa0128c0996a35e3c9ce53
Eric Tillberg <eric.tillberg@gmail.com>
2021-03-09 15:39:44 -0500

    Add Cypress test for auction variant

--- cypress/integration/game_spec.js
+++ cypress/integration/game_spec.js
@@ -1,4 +1,4 @@
-describe("Game", () => {
+describe("Standard game", () => {
   it("works", () => {
     cy.factory("account", {email: "a@b.com", password: "password"})
     cy.login("a@b.com")
@@ -43,6 +43,99 @@ describe("Game", () => {
     cy.contains("Western Mediterranean Sea").click()
     cy.contains("Rome").next().click()
     cy.contains("Spain").click()
+    // FR
+    cy.get("#production1").click()
+    // GB
+    cy.get("#production2").click()
+    // GR
+    cy.get("#production1").click()
+    cy.get("#maneuver2").click()
+    cy.contains("Berlin").next().click()
+    cy.contains("Holland").click()
+    cy.contains("Berlin").next().click()
+    cy.contains("Denmark").click()
+    // RU
+    cy.get("#maneuver2").click()
+    cy.contains("Moscow").next().click()
+    cy.contains("Lemberg").next().click()
+    cy.contains("Enter unfriendly").click()
+  })
+})
 
+describe("Auction game", () => {
+  it("works", () => {
+    cy.factory("account", {email: "a@b.com", password: "password"})
+    cy.login("a@b.com")
+
+    cy.visit("/")
+    cy.contains("Open a New Game").click()
+    cy.contains("Start Auction Variant Game").click()
+
+    // AH
+    cy.get(".bg-AH").contains("2:4").click()
+    cy.get(".bg-AH").contains("3:6").click()
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Do not buy a bond").click()
+    // IT
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Undo").click()
+    cy.get(".bg-IT").contains("2:4").click()
+    cy.get(".bg-IT").contains("3:6").click()
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Do not buy a bond").click()
+    // FR
+    cy.contains("Do not buy a bond").click()
+    cy.get(".bg-FR").contains("2:4").click()
+    cy.get(".bg-FR").contains("3:6").click()
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Do not buy a bond").click()
+    // GB
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Do not buy a bond").click()
+    cy.get(".bg-GB").contains("2:4").click()
+    cy.get(".bg-GB").contains("3:6").click()
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Do not buy a bond").click()
+    // GE
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Do not buy a bond").click()
+    cy.get(".bg-GE").contains("2:4").click()
+    cy.get(".bg-GE").contains("3:6").click()
+    cy.contains("Do not buy a bond").click()
+    // RU
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Do not buy a bond").click()
+    cy.contains("Do not buy a bond").click()
+    cy.get(".bg-RU").contains("2:4").click()
+    cy.get(".bg-RU").contains("3:6").click()
+    // Game starts
+    // AH
+    cy.get("#production1").click()
+    // IT
+    cy.get("#maneuver1").click()
+    cy.contains("Undo").click()
+    cy.get("#production1").click()
+    // FR
+    cy.get("#factory").click()
+    cy.contains("Dijon").next().click()
+    // GB
+    cy.get("#investor").click()
+    cy.get(".bg-AH").contains("1:2").click()
+    cy.get(".bg-AH").contains("4:9").click()
+    // GE
+    cy.get("#import").click()
+    cy.contains("Danzig").next().click()
+    cy.contains("Fleet").click()
+    cy.contains("Berlin").next().click()
+    cy.contains("Berlin").next().click()
+    // RU
+    cy.get("#production2").click()
   })
 })
