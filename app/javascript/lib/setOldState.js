b77e10b31111b90c45068a1add47332cbed7189f
Eric Tillberg <eric.tillberg@gmail.com>
2021-03-14 19:32:46 -0400

    A small refactor

---
+++ app/javascript/lib/setOldState.js
@@ -0,0 +1,49 @@
+import { Nation } from "./constants.js";
+
+export default (game) => {
+  let units = new Map;
+  for (const [key, value] of game.units) {
+    const newValue = new Map;
+    for (const [key2, value2] of value) {
+      newValue.set(key2, Object.assign({}, value2));
+    }
+    units.set(Nation[key.value], newValue);
+  }
+  let nations = new Map();
+  for (const [key, value] of game.nations) {
+    nations.set(Nation[key.value], Object.assign({}, value));
+  }
+  let provinces = new Map();
+  for (const [key, value] of game.provinces) {
+    provinces.set(key, Object.assign({}, value));
+  }
+  let availableBonds = new Set();
+  for (const bond of game.availableBonds) {
+    availableBonds.add(bond);
+  }
+  let availableActions = new Set();
+  for (const action of game.availableActions) {
+    availableActions.add(action);
+  }
+  let players = {};
+  for (const player of Object.keys(game.players)) {
+    players[player] = {};
+    players[player].cash = game.players[player].cash;
+    players[player].name = game.players[player].name;
+    players[player].rawScore = game.players[player].rawScore;
+    let bonds = new Set();
+    for (const bond of game.players[player].bonds) {
+      bonds.add(bond)
+    }
+    players[player].bonds = bonds;
+  }
+
+  return {
+    units,
+    nations,
+    provinces,
+    availableBonds,
+    availableActions,
+    players
+  }
+}
