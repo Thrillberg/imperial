65abd6862f1fa7a31e71c577db789fe5c2e929a5
Eric Tillberg <eric.tillberg@gmail.com>
2021-05-18 15:54:37 -0400

    Add TaxChart
    Fixes #387

--- app/javascript/src/components/TaxChartSlot.vue
+++ app/javascript/src/components/TaxChartSlot.vue
@@ -1,30 +1,68 @@
 <template>
-  <div class="flex justify-evenly items-center h-12">
-    <span class="text-2xl">{{ taxSlot }}</span>
-    <span class="w-24 flex flex-wrap"
-      ><Flag
+  <div class="flex flex-col justify-evenly items-center">
+    <div class="underline">{{ taxSlot }}</div>
+    <div class="flex flex-wrap">
+      <svg
+        width="14px"
+        height="14px"
+        viewBox="0 0 14 14"
+        version="1.1"
         v-for="nation in nations"
-        v-bind:nation="nation"
-        height="15"
-        v-bind:class="'my-1 mx-auto'"
-        v-bind:key="nation"
-      ></Flag
-    ></span>
-    <span class="text-2xl">+{{ taxSlot - 5 }}</span>
+        :key="nation"
+        class="m-0.5"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+      >
+        <g>
+          <circle
+            :cx="7"
+            :cy="7"
+            r="7"
+            stroke-width="0.5"
+            stroke="black"
+            :fill="fill(nation)"
+          ></circle>
+        </g>
+      </svg>
+    </div>
+    <div>+ {{ taxSlot - 5 }}</div>
   </div>
 </template>
 
 <script>
-import Flag from "./flags/Flag.vue";
-
 export default {
   name: "TaxChartSlot",
-  components: {
-    Flag
-  },
   props: {
     taxSlot: Number,
     nations: Array
+  },
+  methods: {
+    fill(nation) {
+      switch (nation) {
+        case "AH":
+          return "#EBE084"
+        case "IT":
+          return "#6E8D4E"
+        case "FR":
+          return "#54BFF9"
+        case "GB":
+          return "#EF7F72"
+        case "GE":
+          return "#8F8F8F"
+        case "RU":
+          return "#9C6BAE"
+        case "CN":
+          return "#ebe084"
+        case "IN":
+          return "silver"
+        case "BR":
+          return "#6E8D4E"
+        case "US":
+          return "#EF7F72"
+        case "EU":
+          return "#54bff9"
+      }
+    }
   }
 };
 </script>
