7feb9821bcf8cc86018e3732f4a8ecdcfcf13f92
Eric Tillberg <eric.tillberg@gmail.com>
2021-07-03 19:39:08 -0400

    try adding google analytics

--- config/webpack/rules/vue.js
+++ config/webpack/rules/vue.js
@@ -44,7 +44,26 @@ module.exports = {
         viewport: "width=device-width, initial-scale=1"
       },
       template: require('html-webpack-template'),
-      title: "Imperial"
+      title: "Imperial",
+      googleAnalytics: {
+        trackingId: "G-HETMN2G3GE",
+        pageViewOnLoad: true
+      },
+      headHtmlSnippet: `
+        <!-- Google Tag Manager -->
+          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TPLXHT2');</script>
+        <!-- End Google Tag Manager -->
+      `,
+      bodyHtmlSnippet: `
+        <!-- Google Tag Manager (noscript) -->
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TPLXHT2"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+      `
     }),
     new HtmlWebpackHarddiskPlugin({
       outputPath: path.resolve(__dirname, '../../../public/packs')
