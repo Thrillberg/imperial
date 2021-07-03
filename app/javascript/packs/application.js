74928502addd14bf55475d13285afc725f89e0bf
Eric Tillberg <eric.tillberg@gmail.com>
2021-07-03 18:12:32 -0400

    update package-lock.json

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,6 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 
 import Vue from "vue";
