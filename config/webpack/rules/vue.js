b7b2f72098852f3c05a2d5c0a3ae43c851c91ab6
Eric Tillberg <eric.tillberg@gmail.com>
2021-07-09 20:45:29 -0400

    Google Analytics only on production

--- config/webpack/rules/vue.js
+++ config/webpack/rules/vue.js
@@ -1,5 +1,4 @@
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin')
 const path = require('path')
 
@@ -31,40 +30,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      alwaysWriteToDisk: true,
-      appMountId: "app",
-      favicon: "favicon.ico",
-      inject: "body",
-      lang: 'en-US',
-      meta: {
-        charset: "UTF-8",
-        description: "Play Imperial online",
-        keywords: "imperial, board game, game, online, play",
-        viewport: "width=device-width, initial-scale=1"
-      },
-      template: require('html-webpack-template'),
-      title: "Imperial",
-      googleAnalytics: {
-        trackingId: "G-HETMN2G3GE",
-        pageViewOnLoad: true
-      },
-      headHtmlSnippet: `
-        <!-- Google Tag Manager -->
-          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-TPLXHT2');</script>
-        <!-- End Google Tag Manager -->
-      `,
-      bodyHtmlSnippet: `
-        <!-- Google Tag Manager (noscript) -->
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TPLXHT2"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
-      `
-    }),
     new HtmlWebpackHarddiskPlugin({
       outputPath: path.resolve(__dirname, '../../../public/packs')
     }),
