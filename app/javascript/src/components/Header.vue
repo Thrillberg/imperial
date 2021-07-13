c668c2d5c36c88a6d3934e756fc8a4e814b491a7
Eric Tillberg <eric.tillberg@gmail.com>
2021-07-13 14:05:52 -0400

    Make header more mobile-friendly

--- app/javascript/src/components/Header.vue
+++ app/javascript/src/components/Header.vue
@@ -1,56 +1,56 @@
 <template>
   <header>
-    <div class="bg-green-200 flex justify-between px-8">
+    <div class="bg-green-200 flex justify-center sm:justify-between flex-wrap sm:flex-nowrap text-sm sm:px-8 sm:text-base">
       <div class="flex">
         <router-link
           :to="{ path: '/' }"
-          class="self-center pr-8 underline"
+          class="self-center underline px-1 sm:pr-8"
         >
           Home
         </router-link>
         <router-link
           :to="{ path: '/rules' }"
-          class="self-center pr-8 underline"
+          class="self-center underline px-1 sm:pr-8"
         >
           How to play
         </router-link>
         <router-link
           :to="{ path: '/about' }"
-          class="self-center pr-8 underline"
+          class="self-center underline px-1 sm:pr-8"
         >
           About
         </router-link>
-        <a href="https://forms.gle/oW4wZeco91HUqeDg8" class="self-center pr-8 underline">
+        <a href="https://forms.gle/oW4wZeco91HUqeDg8" class="self-center underline px-1 sm:pr-8">
           Feedback survey
         </a>
-        <div v-for="(error, index) in errors" v-bind:key="index" class="text-red-700">
+        <div v-for="(error, index) in errors" v-bind:key="index" class="text-red-700 px-1 sm:pr-8">
           <b>{{ error }}</b>
         </div>
       </div>
-      <div class="flex justify-evenly">
-        <span v-if="profile.anonymity_confirmed_at && !profile.registered" class="self-center mr-10">Playing as {{ profile.username }}</span>
-        <span v-if="profile.email" class="self-center mr-10">Signed in as
+      <div class="flex">
+        <span v-if="profile.anonymity_confirmed_at && !profile.registered" class="self-center px-1 sm:px-8">Playing as {{ profile.username }}</span>
+        <span v-if="profile.email" class="self-center px-1 sm:px-8">Signed in as
           <router-link :to="{ path: '/users/' + profile.id }" class="underline">
             {{ profile.username }}
           </router-link>
         </span>
         <button
           v-if="!profile.email"
-          class="rounded py-2 px-6 my-4 bg-green-800 text-white cursor-pointer"
+          class="rounded py-2 px-1 sm:px-6 my-1 sm:my-4 bg-green-800 text-white cursor-pointer"
           @click="signIn"
         >
           Sign In
         </button>
         <button
           v-if="!profile.email"
-          class="rounded py-2 px-6 my-4 bg-green-800 text-white cursor-pointer ml-5"
+          class="rounded py-2 px-1 sm:px-6 my-1 sm:my-4 bg-green-800 text-white cursor-pointer ml-5"
           @click="register"
         >
           Register
         </button>
         <button
           v-if="profile.email"
-          class="rounded py-2 px-6 my-4 bg-green-800 text-white cursor-pointer"
+          class="rounded py-2 px-1 sm:px-6 my-1 sm:my-4 bg-green-800 text-white cursor-pointer"
           @click="signOut"
           >
           Sign Out
