056088a3d0c59013653801b1aef6e039a931da5b
Eric Tillberg <eric.tillberg@gmail.com>
2021-10-03 14:00:11 -0400

    Display finished games at bottom of YourGames list
    Fixes #531

--- app/javascript/src/components/UnstartedGameList.vue
+++ app/javascript/src/components/UnstartedGameList.vue
@@ -9,7 +9,7 @@
       <div class="hidden sm:w-1/5 sm:inline-block mx-2"></div>
     </div>
     <div v-for="game of games" v-bind:key="game.id">
-      <router-link :to="{ path: '/game/' + game.id }" class="flex justify-between items-center hover:bg-gray-200 py-2">
+      <router-link :to="{ path: '/game/' + game.id }" class="flex justify-between items-center hover:bg-yellow-100 py-2">
         <div class="w-1/3 sm:w-1/5 mx-2">{{ game.name }}</div>
         <div class="hidden sm:w-1/5 sm:inline-block mx-2">{{ game.players.length }}</div>
         <div class="w-1/3 sm:w-1/5 mx-2">{{ game.host }}</div>
