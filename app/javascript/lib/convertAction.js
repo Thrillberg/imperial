13990f859a34a8a5b5e3fd4e8e59f1e648b52314
Eric Tillberg <eric.tillberg@gmail.com>
2022-01-12 20:16:04 -0500

    Add convertAction and fix deserializer

---
+++ app/javascript/lib/convertAction.js
@@ -0,0 +1,51 @@
+import { Nation, Nation2030 } from "../lib/constants.js";
+
+export default function(action, baseGame) {
+  if (
+    action.type === "initialize" &&
+    (!action.payload.variant || action.payload.variant === "standard")
+  ) {
+    if (baseGame === "imperial") {
+      action.payload.players = action.payload.players.map(player => {
+        return {
+          id: player.id,
+          nation: Nation[player.nation.value]
+        };
+      });
+    } else if (baseGame === "imperial2030") {
+      action.payload.players = action.payload.players.map(player => {
+        return {
+          id: player.id,
+          nation: Nation2030[player.nation.value]
+        };
+      });
+    }
+  } else if (
+    (
+      action.type === "rondel" ||
+      action.type === "bondPurchase" ||
+      action.type === "skipBondPurchase" ||
+      action.type === "skipBuildFactory"
+    ) && action.payload.nation
+  ) {
+    if (baseGame === "imperial") {
+      action.payload.nation = Nation[action.payload.nation.value];
+    } else if (baseGame === "imperial2030") {
+      action.payload.nation = Nation2030[action.payload.nation.value];
+    }
+  } else if (
+    action.type === "fight" ||
+    action.type === "coexist" ||
+    action.type === "unfriendlyEntrance" ||
+    action.type === "friendlyEntrance"
+  ) {
+    if (baseGame === "imperial") {
+      action.payload.incumbent = Nation[action.payload.incumbent.value];
+      action.payload.challenger = Nation[action.payload.challenger.value];
+    } else if (baseGame === "imperial2030") {
+      action.payload.incumbent = Nation2030[action.payload.incumbent.value];
+      action.payload.challenger = Nation2030[action.payload.challenger.value];
+    }
+  }
+  return action;
+}
--- app/javascript/lib/deserializeGame.js
+++ app/javascript/lib/deserializeGame.js
@@ -1,13 +1,79 @@
-import { Nation, Nation2030 } from "./constants.js";
+import { Bond, Nation, Nation2030 } from "./constants.js";
+import convertAction from "./convertAction.js";
 
 export default (serializedGame) => {
   const parsedGame = eval('(' + serializedGame + ')');
+  const baseGame = parsedGame.baseGame;
   if (parsedGame) {
     let game = Object.assign({}, parsedGame);
-    game.availableActions = new Set(parsedGame.availableActions);
-    game.units = new Map(parsedGame.units);
-    // TODO: We need to deserialize the other 5 keys (unitLimits, nations, provinces, availableBonds, players' bonds) AND be sure to use the correct Nation type wherever necessary.
-    console.log(game)
+    game.availableActions = new Set();
+    parsedGame.availableActions.forEach(action => {
+      game.availableActions.add(convertAction(action, baseGame));
+    });
+    game.units = new Map();
+    parsedGame.units.forEach(unit => {
+      const [nation, nationsUnits] = unit;
+      if (baseGame === "imperial") {
+        game.units.set(Nation[nation.value], nationsUnits);
+      } else if (baseGame === "imperial2030") {
+        game.units.set(Nation2030[nation.value], nationsUnits);
+      }
+    });
+    game.unitLimits = new Map();
+    parsedGame.unitLimits.forEach(unitLimit => {
+      const [nation, nationsUnitLimits] = unitLimit;
+      if (baseGame === "imperial") {
+        game.unitLimits.set(Nation[nation.value], nationsUnitLimits);
+      } else if (baseGame === "imperial2030") {
+        game.unitLimits.set(Nation2030[nation.value], nationsUnitLimits);
+      }
+    });
+    game.nations = new Map();
+    parsedGame.nations.forEach(nationData => {
+      const [nation, data] = nationData;
+      if (baseGame === "imperial") {
+        game.nations.set(Nation[nation.value], data);
+      } else if (baseGame === "imperial2030") {
+        game.nations.set(Nation2030[nation.value], data);
+      }
+    });
+    game.provinces = new Map();
+    parsedGame.provinces.forEach(provinceData => {
+      const [province, data] = provinceData;
+      // Do we need to account for data.flag being a Nation??
+      game.provinces.set(province, data);
+    });
+    game.availableBonds = new Set();
+    parsedGame.availableBonds.forEach(bond => {
+      const {nation, number, cost} = bond;
+      if (baseGame === "imperial") {
+        game.availableBonds.add(Bond(Nation[nation.value], number));
+      } else if (baseGame === "imperial2030") {
+        game.availableBonds.add(Bond(Nation2030[nation.value], number));
+      }
+    });
+    game.players = {};
+    Object.keys(parsedGame.players).forEach(player => {
+      if (baseGame === "imperial") {
+        let bonds = new Set();
+        parsedGame.players[player].bonds.forEach(bond => {
+          bonds.add(Bond(Nation[bond.nation.value], bond.number));
+        });
+        game.players[player] = {
+          ...parsedGame.players[player],
+          bonds
+        }
+      } else if (baseGame === "imperial2030") {
+        let bonds = new Set();
+        parsedGame.players[player].bonds.forEach(bond => {
+          bonds.add(Bond(Nation2030[bond.nation.value], bond.number));
+        });
+        game.players[player] = {
+          ...parsedGame.players[player],
+          bonds
+        }
+      }
+    });
     return game
   }
   return serializedGame;
