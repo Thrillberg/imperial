e6e630246f676fa5b61de64bc40665a2558bf688
Eric Tillberg <eric.tillberg@gmail.com>
2022-01-31 15:07:56 -0500

    Add Last Move At column to Games and User components

--- app/javascript/src/views/Games.vue
+++ app/javascript/src/views/Games.vue
@@ -3,16 +3,20 @@
     <b>All Games</b>
     <div class="flex border-b border-black mt-2">
       <div class="w-1/2"><b>Name</b></div>
+      <div class="w-1/2"><b>Last Move At</b></div>
     </div>
     <div v-for="game of games" v-bind:key="game.id">
       <router-link :to="{ path: '/game/' + game.id }" class="flex justify-between items-center hover:bg-gray-200 py-2">
         <div class="w-1/2">{{ game.name }}</div>
+        <div class="w-1/2">{{ toDate(game.last_move_at) }}</div>
       </router-link>
     </div>
   </div>
 </template>
 
 <script>
+import { DateTime } from "luxon";
+
 export default {
   name: "Games",
   props: { profile: Object },
@@ -25,6 +29,11 @@ export default {
     fetch("/get_games", { method: "GET" })
       .then(response => response.json())
       .then(data => this.games = data.filter(game => !game.cancelled_at && !game.cloned_from_game))
+  },
+  methods: {
+    toDate(timestamp) {
+      return DateTime.fromISO(timestamp).toLocaleString();
+    }
   }
 }
 </script>
--- app/javascript/src/views/User.vue
+++ app/javascript/src/views/User.vue
@@ -48,15 +48,17 @@
       </p>
       <b>{{ user.name }}'s Finished Games</b>
       <div class="flex border-b border-black mt-2">
-        <div class="w-1/3"><b>Name</b></div>
-        <div class="w-1/3"><b>Players</b></div>
-        <div class="w-1/3"><b>Winner</b></div>
+        <div class="w-1/4"><b>Name</b></div>
+        <div class="w-1/4"><b>Players</b></div>
+        <div class="w-1/4"><b>Winner</b></div>
+        <div class="w-1/4"><b>Finished On</b></div>
       </div>
       <div v-for="game of finishedGames" :key="game.id">
         <router-link :to="{ path: '/game/' + game.id }" class="flex justify-between items-center hover:bg-gray-200 py-2">
-          <div class="w-1/3">{{ game.name }}</div>
-          <div class="w-1/3">{{ game.players.length }}</div>
-          <div class="w-1/3">{{ game.winner_name }}</div>
+          <div class="w-1/4">{{ game.name }}</div>
+          <div class="w-1/4">{{ game.players.length }}</div>
+          <div class="w-1/4">{{ game.winner_name }}</div>
+          <div class="w-1/4">{{ toDate(game.last_move_at) }}</div>
         </router-link>
       </div>
     </div>
@@ -67,6 +69,8 @@
 </template>
 
 <script>
+import { DateTime } from "luxon";
+
 export default {
   name: "User",
   created() {
@@ -116,6 +120,9 @@ export default {
     },
     resetTurnNotifications() {
       this.successfullyUpdated = false;
+    },
+    toDate(timestamp) {
+      return DateTime.fromISO(timestamp).toLocaleString();
     }
   }
 }
