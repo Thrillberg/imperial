989dfe9985ca023560b9ddf80027f9d49654e0c0
Eric Tillberg <eric.tillberg@gmail.com>
2022-11-05 09:16:19 -0400

    Linting. Nothing to see here.

--- app/javascript/src/components/ControlPanel.vue
+++ app/javascript/src/components/ControlPanel.vue
@@ -8,41 +8,45 @@
       Undo
     </button>
     <div class="flex flex-wrap">
-      <AvailableBonds 
+      <AvailableBonds
         v-if="!purchasingBond"
-        :game="game">
-      </AvailableBonds>
-      <TaxStatus :game="game"></TaxStatus>
+        :game="game"
+      />
+      <TaxStatus :game="game" />
     </div>
     <div v-if="!paused">
       <div
-        v-if="game.importing && !chooseImportType && (profile.username === controllingPlayerName || (game.soloMode && hostingThisGame))"
+        v-if="game.importing
+          && !chooseImportType
+          && (profile.username === controllingPlayerName || (game.soloMode && hostingThisGame))"
         class="text-center inline-flex flex-col"
       >
         <button
-          @click="$emit('runImport')"
           class="rounded py-2 px-6 m-1 sm:m-4 bg-red-500 text-white cursor-pointer"
+          @click="$emit('runImport')"
         >
           End import
         </button>
         <div class="m-2">
           You have
-          <b>{{ this.game.maxImports - importPlacements.length }}</b> imports left.
+          <b>{{ game.maxImports - importPlacements.length }}</b> imports left.
         </div>
       </div>
       <div
-        v-if="game.importing && !!chooseImportType && (profile.username === controllingPlayerName || (game.soloMode && hostingThisGame))"
+        v-if="game.importing
+          && !!chooseImportType
+          && (profile.username === controllingPlayerName || (game.soloMode && hostingThisGame))"
         class="text-center text-lg"
       >
         <button
-          @click="$emit('chooseImportType', 'army')"
           class="rounded p-2 m-1 sm:m-4 bg-green-800 text-white cursor-pointer"
+          @click="$emit('chooseImportType', 'army')"
         >
           Army
         </button>
         <button
-          @click="$emit('chooseImportType', 'fleet')"
           class="rounded p-2 m-1 sm:m-4 bg-green-800 text-white cursor-pointer"
+          @click="$emit('chooseImportType', 'fleet')"
         >
           Fleet
         </button>
@@ -50,30 +54,55 @@
       </div>
       <button
         v-if="canEndManeuver"
-        v-on:click="endManeuver"
         class="rounded py-2 px-6 m-1 sm:m-4 bg-red-500 text-white cursor-pointer self-start"
+        @click="endManeuver"
       >
         End maneuver
       </button>
-      <ConflictHandler :game="game" :profile="profile" :controllingPlayerName="controllingPlayerName" :hostingThisGame="hostingThisGame" v-on:tick-with-action="tickWithAction"></ConflictHandler>
-      <div class="text-center" v-if="canForceInvestor">
-        <button @click="forceInvestor" class="rounded p-2 m-1 sm:m-4 bg-green-800 text-white cursor-pointer">
+      <ConflictHandler
+        :game="game"
+        :profile="profile"
+        :controlling-player-name="controllingPlayerName"
+        :hosting-this-game="hostingThisGame"
+        @tick-with-action="tickWithAction"
+      />
+      <div
+        v-if="canForceInvestor"
+        class="text-center"
+      >
+        <button
+          class="rounded p-2 m-1 sm:m-4 bg-green-800 text-white cursor-pointer"
+          @click="forceInvestor"
+        >
           Force investor
         </button>
-        <button @click="skipForceInvestor" class="rounded p-2 m-1 sm:m-4 bg-green-800 text-white cursor-pointer">
+        <button
+          class="rounded p-2 m-1 sm:m-4 bg-green-800 text-white cursor-pointer"
+          @click="skipForceInvestor"
+        >
           Do not force investor
         </button>
       </div>
-      <div class="text-center" v-if="canBlockCanal">
-        <button @click="blockCanal" class="rounded p-2 m-1 sm:m-4 bg-green-800 text-white cursor-pointer">
+      <div
+        v-if="canBlockCanal"
+        class="text-center"
+      >
+        <button
+          class="rounded p-2 m-1 sm:m-4 bg-green-800 text-white cursor-pointer"
+          @click="blockCanal"
+        >
           Block canal
         </button>
-        <button @click="unblockCanal" class="rounded p-2 m-1 sm:m-4 bg-green-800 text-white cursor-pointer">
+        <button
+          class="rounded p-2 m-1 sm:m-4 bg-green-800 text-white cursor-pointer"
+          @click="unblockCanal"
+        >
           Do not block canal
         </button>
       </div>
       <button
-        v-if="game.buildingFactory && (profile.username === controllingPlayerName || (game.soloMode && hostingThisGame))"
+        v-if="game.buildingFactory
+          && (profile.username === controllingPlayerName || (game.soloMode && hostingThisGame))"
         class="rounded py-2 px-6 m-1 sm:m-4 bg-green-800 text-white cursor-pointer"
         @click="$emit('skipBuildFactory')"
       >
@@ -84,17 +113,25 @@
         :game="game"
         :current_player="controllingPlayerName"
         :profile="profile"
-        :tradedInValue="tradedInValue"
-        @purchaseBond="purchaseBond"
-        @skip="this.skipPurchaseBond"
-      ></BondPurchase>
+        :traded-in-value="tradedInValue"
+        @purchase-bond="purchaseBond"
+        @skip="skipPurchaseBond"
+      />
       <div v-if="destroyingFactory()">
-        <div class="text-lg">Do you want to destroy the factory at <b>{{ this.factoryToDestroy }}</b>?</div>
+        <div class="text-lg">
+          Do you want to destroy the factory at <b>{{ factoryToDestroy }}</b>?
+        </div>
         <div class="flex flex-wrap justify-evenly">
-          <button @click="destroyFactory" class="rounded p-2 m-1 sm:m-4 bg-green-800 text-white cursor-pointer inline-block mt-8">
+          <button
+            class="rounded p-2 m-1 sm:m-4 bg-green-800 text-white cursor-pointer inline-block mt-8"
+            @click="destroyFactory"
+          >
             Yes
           </button>
-          <button @click="skipDestroyFactory" class="rounded p-2 m-1 sm:m-4 bg-green-800 text-white cursor-pointer inline-block mt-8">
+          <button
+            class="rounded p-2 m-1 sm:m-4 bg-green-800 text-white cursor-pointer inline-block mt-8"
+            @click="skipDestroyFactory"
+          >
             No
           </button>
         </div>
@@ -104,56 +141,71 @@
 </template>
 
 <script>
-import Action from "../../lib/action.js";
+import Action from '../../lib/action';
 
-import AvailableBonds from "../components/AvailableBonds.vue";
-import BondPurchase from "../components/BondPurchase.vue";
-import ConflictHandler from "../components/ConflictHandler.vue";
-import TaxStatus from "../components/TaxStatus.vue";
+import AvailableBonds from './AvailableBonds.vue';
+import BondPurchase from './BondPurchase.vue';
+import ConflictHandler from './ConflictHandler.vue';
+import TaxStatus from './TaxStatus.vue';
 
 export default {
-  name: "ControlPanel",
-  components: { AvailableBonds, BondPurchase, ConflictHandler, TaxStatus },
-  props: [
-    "game",
-    "chooseImportType",
-    "controllingPlayerName",
-    "paused",
-    "profile",
-    "importPlacements",
-    "gameData",
-    "tradedInBondNation",
-    "tradedInValue",
-    "hostingThisGame",
-  ],
+  name: 'ControlPanel',
+  components: {
+    AvailableBonds, BondPurchase, ConflictHandler, TaxStatus,
+  },
+  props: {
+    game: { type: Object, default: () => {} },
+    chooseImportType: { type: String, default: '' },
+    controllingPlayerName: { type: String, default: '' },
+    paused: { type: Boolean },
+    profile: { type: Object, default: () => {} },
+    importPlacements: { type: Array, default: () => [] },
+    gameData: { type: Object, default: () => {} },
+    tradedInBondNation: { type: String, default: '' },
+    tradedInValue: { type: Number, default: 0 },
+    hostingThisGame: { type: Boolean },
+  },
+  emits: ['runImport', 'chooseImportType', 'skipBuildFactory', 'purchaseBond', 'endManeuver', 'tick'],
+  data() {
+    return { factoryToDestroy: '' };
+  },
   computed: {
     purchasingBond() {
-      const purchasingBond = this.game.availableActions.size > 0 &&
-        Array.from(this.game.availableActions).every(
-          (action) => action.type === "bondPurchase" || action.type === "skipBondPurchase" || action.type === "undo"
+      const purchasingBond = this.game.availableActions.size > 0
+        && Array.from(this.game.availableActions).every(
+          (action) => action.type === 'bondPurchase' || action.type === 'skipBondPurchase' || action.type === 'undo',
         );
-      return purchasingBond && (this.profile.username === this.controllingPlayerName || (this.game.soloMode && this.hostingThisGame));
+      return (
+        purchasingBond
+        && (
+          this.profile.username === this.controllingPlayerName
+          || (this.game.soloMode && this.hostingThisGame)
+        )
+      );
     },
-    canForceInvestor: function () {
-      if (this.game.availableActions.size > 0 &&
-        Array.from(this.game.availableActions).every((action) => action.type === "forceInvestor" || action.type === "skipForceInvestor" || action.type === "undo")) {
-          if (this.game.swissBanks.includes(this.profile.username) || (this.game.soloMode && this.hostingThisGame)) {
-            return true;
-          }
+    canForceInvestor() {
+      if (this.game.availableActions.size > 0
+        && Array.from(this.game.availableActions).every((action) => action.type === 'forceInvestor' || action.type === 'skipForceInvestor' || action.type === 'undo')) {
+        if (
+          this.game.swissBanks.includes(this.profile.username)
+          || (this.game.soloMode && this.hostingThisGame)
+        ) {
+          return true;
+        }
       }
       return false;
     },
-    canBlockCanal: function () {
+    canBlockCanal() {
       if (
         Array.from(this.game.availableActions).every(
-          (action) => action.type === "blockCanal" ||
-            action.type === "unblockCanal" ||
-            action.type === "undo"
+          (action) => action.type === 'blockCanal'
+            || action.type === 'unblockCanal'
+            || action.type === 'undo',
         )
       ) {
         if (
-          this.profile.username === this.controllingPlayerName ||
-          this.game.soloMode && this.hostingThisGame
+          this.profile.username === this.controllingPlayerName
+          || (this.game.soloMode && this.hostingThisGame)
         ) {
           return true;
         }
@@ -161,77 +213,75 @@ export default {
       return false;
     },
     canEndManeuver() {
-      // We don't handle the case where control has passed because someone can block a canal so there is the following exception.
+      // We don't handle the case where control has passed because someone can block a canal
+      // so there is the following exception.
       if (
         Array.from(this.game.availableActions).every(
-          (action) => action.type === "blockCanal" ||
-            action.type === "unblockCanal" ||
-            action.type === "undo"
+          (action) => action.type === 'blockCanal'
+            || action.type === 'unblockCanal'
+            || action.type === 'undo',
         )
       ) {
-        return false
+        return false;
       }
 
-      return this.game.maneuvering &&
-      this.factoryToDestroy === "" &&
-      !this.game.handlingConflict &&
-      (
-        this.profile.username === this.game.currentPlayerName ||
-          (this.game.soloMode && this.hostingThisGame)
-      )
-    }
-  },
-  data() {
-    return { factoryToDestroy: "" }
+      return this.game.maneuvering
+      && this.factoryToDestroy === ''
+      && !this.game.handlingConflict
+      && (
+        this.profile.username === this.game.currentPlayerName
+          || (this.game.soloMode && this.hostingThisGame)
+      );
+    },
   },
   methods: {
     purchaseBond(bond) {
-      this.$emit("purchaseBond", bond);
+      this.$emit('purchaseBond', bond);
     },
     skipPurchaseBond() {
       for (const action of this.game.availableActions) {
-        if (action.type === "skipBondPurchase") {
+        if (action.type === 'skipBondPurchase') {
           this.tickWithAction(action);
         }
       }
     },
-    endManeuver: function() {
-      this.$emit("endManeuver", Action.endManeuver());
+    endManeuver() {
+      this.$emit('endManeuver', Action.endManeuver());
     },
-    tickWithAction: function(action) {
-      this.$emit("tick", action);
+    tickWithAction(action) {
+      this.$emit('tick', action);
     },
-    destroyingFactory: function () {
+    destroyingFactory() {
       if (
-        this.profile.username !== this.controllingPlayerName &&
-        !(this.game.soloMode && this.hostingThisGame)
+        this.profile.username !== this.controllingPlayerName
+        && !(this.game.soloMode && this.hostingThisGame)
       ) {
-        return false
+        return false;
       }
 
       let destroyingFactory = false;
       for (const action of this.game.availableActions) {
-        if (action.type === "destroyFactory" || action.type === "skipDestroyFactory") {
+        if (action.type === 'destroyFactory' || action.type === 'skipDestroyFactory') {
           destroyingFactory = true;
           this.factoryToDestroy = action.payload.province;
         }
       }
-      return destroyingFactory
+      return destroyingFactory;
     },
-    destroyFactory: function() {
+    destroyFactory() {
       let destroyAction = {};
       for (const action of this.game.availableActions) {
-        if (action.type === "destroyFactory") {
+        if (action.type === 'destroyFactory') {
           destroyAction = action;
         }
       }
       this.tickWithAction(destroyAction);
-      this.factoryToDestroy = "";
+      this.factoryToDestroy = '';
     },
-    skipDestroyFactory: function() {
+    skipDestroyFactory() {
       let skipAction = {};
       for (const action of this.game.availableActions) {
-        if (action.type === "skipDestroyFactory") {
+        if (action.type === 'skipDestroyFactory') {
           skipAction = action;
         }
       }
@@ -243,9 +293,9 @@ export default {
       let canUndo = false;
       for (const action of this.game.availableActionsWithUndo()) {
         if (
-          this.game.log.length > 1 && action.type === "undo" && (
-            action.payload.player === this.profile.username ||
-            (this.game.soloMode && this.hostingThisGame)
+          this.game.log.length > 1 && action.type === 'undo' && (
+            action.payload.player === this.profile.username
+            || (this.game.soloMode && this.hostingThisGame)
           )
         ) {
           canUndo = true;
@@ -256,8 +306,9 @@ export default {
     undo() {
       for (const action of this.game.availableActionsWithUndo()) {
         if (
-          action.type === "undo" && (
-            action.payload.player === this.profile.username || (this.game.soloMode && this.hostingThisGame)
+          action.type === 'undo' && (
+            action.payload.player === this.profile.username
+            || (this.game.soloMode && this.hostingThisGame)
           )
         ) {
           this.tickWithAction(action);
@@ -266,32 +317,32 @@ export default {
     },
     forceInvestor() {
       for (const action of this.game.availableActions) {
-        if (action.type === "forceInvestor") {
+        if (action.type === 'forceInvestor') {
           this.tickWithAction(action);
         }
       }
     },
     skipForceInvestor() {
       for (const action of this.game.availableActions) {
-        if (action.type === "skipForceInvestor") {
+        if (action.type === 'skipForceInvestor') {
           this.tickWithAction(action);
         }
       }
     },
     blockCanal() {
       for (const action of this.game.availableActions) {
-        if (action.type === "blockCanal") {
+        if (action.type === 'blockCanal') {
           this.tickWithAction(action);
         }
       }
     },
     unblockCanal() {
       for (const action of this.game.availableActions) {
-        if (action.type === "unblockCanal") {
+        if (action.type === 'unblockCanal') {
           this.tickWithAction(action);
         }
       }
     },
-  }
+  },
 };
 </script>
