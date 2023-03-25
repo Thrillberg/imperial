6f69e3cb446278c1604d1af03f9504cac82a7389
Eric Tillberg <eric.tillberg@gmail.com>
2023-03-25 12:54:06 -0400

    Reduce Sentry sample rate so txs don't get dropped at end of month

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,6 @@ Sentry.init do |config|
   config.dsn = "https://fd248a7ee8904fec99cfee9a4ea6f51c@o987046.ingest.sentry.io/5943913"
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  config.traces_sample_rate = 0.2
+  config.traces_sample_rate = 0.1
   config.environment = ENV["RAILS_ENV"]
 end
