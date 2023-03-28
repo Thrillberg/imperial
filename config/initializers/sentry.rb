0282558d25fa7bd370b83d906c0bfddd40bbb99b
Eric Tillberg <eric.tillberg@gmail.com>
2023-03-27 20:27:30 -0400

    Only use Sentry in production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,9 @@
 Sentry.init do |config|
-  config.dsn = "https://fd248a7ee8904fec99cfee9a4ea6f51c@o987046.ingest.sentry.io/5943913"
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  if Rails.env == "production"
+    config.dsn = "https://fd248a7ee8904fec99cfee9a4ea6f51c@o987046.ingest.sentry.io/5943913"
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  config.traces_sample_rate = 0.1
-  config.environment = ENV["RAILS_ENV"]
+    config.traces_sample_rate = 0.1
+    config.environment = Rails.env
+  end
 end
