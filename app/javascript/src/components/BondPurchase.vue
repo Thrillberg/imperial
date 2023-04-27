7397e5704a9965aff0b4520099fc4e61c9315e9e
Eric Tillberg <eric.tillberg@gmail.com>
2023-04-27 12:30:02 -0400

    Fix ugly bug with auction

--- app/javascript/src/components/BondPurchase.vue
+++ app/javascript/src/components/BondPurchase.vue
@@ -37,7 +37,7 @@
       </v-card-title>
       <v-card-text>
         <v-row>
-          <v-col :cols="game.auction.inAuction ? '12' : '9'">
+          <v-col :cols="game.auction?.inAuction ? '12' : '9'">
             <v-sheet
               v-for="bond of game.availableBonds"
               :key="bond.nation+bond.cost"
@@ -59,11 +59,11 @@
             </v-sheet>
           </v-col>
           <v-divider
-            v-if="!game.auction.inAuction"
+            v-if="!game.auction?.inAuction"
             vertical
           />
           <v-col
-            v-if="!game.auction.inAuction"
+            v-if="!game.auction?.inAuction"
             cols="3"
           >
             Your bonds that can be upgraded
