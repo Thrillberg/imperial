12c2aa88d38cfc049a72fcd5fd7646eb8537653b
Eric Tillberg <eric.tillberg@gmail.com>
2023-05-02 21:22:51 -0400

    restore some code we probably need

--- app/javascript/ml/machineLearning.js
+++ app/javascript/ml/machineLearning.js
@@ -7,6 +7,66 @@ const fs = require("fs");
 
 export default class MachineLearning {
 // class MachineLearning {
+  static stateArray(game, playerName) {
+    let stateArray = [];
+    // stateArray[0] = AH's rondel position
+    // stateArray[1] = IT's rondel position
+    // stateArray[2] = FR's rondel position
+    // stateArray[3] = GB's rondel position
+    // stateArray[4] = GE's rondel position
+    // stateArray[5] = RU's rondel position
+    // stateArray[6] = AH's power points
+    // stateArray[7] = IT's power points
+    // stateArray[8] = FR's power points
+    // stateArray[9] = GB's power points
+    // stateArray[10] = GE's power points
+    // stateArray[11] = RU's power points
+    // stateArray[12] = Number of available bonds
+    // stateArray[13] = player1's cash
+    // stateArray[14] = player1's rawScore
+    // stateArray[15] = player1's bond count
+    let index = 0;
+    for (const [, data] of game.nations) {
+      stateArray[index] = this.rondelPositionIndex(data.rondelPosition);
+      index += 1;
+    }
+    for (const [, data] of game.nations) {
+      stateArray[index] = data.powerPoints;
+      index += 1;
+    }
+    stateArray[index] = game.availableBonds.size;
+    index += 1;
+    let player = game.players[playerName];
+    stateArray[index] = player.cash;
+    index += 1;
+    stateArray[index] = player.rawScore;
+    index += 1;
+    stateArray[index] = player.bonds.size;
+    return stateArray;
+  }
+
+  static rondelPositionIndex(name) {
+    if (name === "taxation") {
+      return 0;
+    } else if (name === "factory") {
+      return 1;
+    } else if (name === "production1") {
+      return 2;
+    } else if (name === "maneuver1") {
+      return 3;
+    } else if (name === "investor") {
+      return 4;
+    } else if (name === "import") {
+      return 5;
+    } else if (name === "production2") {
+      return 6;
+    } else if (name === "maneuver2") {
+      return 7;
+    } else {
+      return -1
+    }
+  }
+
   static async predictSample(sample) {
     const model = await tf.loadLayersModel(
       //"../ml_models"
@@ -58,7 +118,7 @@ const csvTransform = ({
   Cash,
   RawScore,
   NumberOfOwnedBonds,
-  annotation,
+  annotation
 }) => {
   const xs = [
     normalize(AHRondelPosition, 0, 7),
@@ -114,4 +174,4 @@ async function run(epochCount, savePath) {
   }
 }
 
-run(6, 'file:///Users/eric/code/imperial/app/javascript/ml/ml_models');
+run(6, 'file:///Users/eric/code/imperial/app/javascript/ml/ml_models')
