a0c4302c06c42187ae2202e519352b34033291a8
Eric Tillberg <Thrillberg@users.noreply.github.com>
2024-12-26 17:44:27 -0500

    Set maxAssetSize

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker');
 
+environment.config.merge({
+  performance: {
+    maxAssetSize: 500000,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+});
+
 module.exports = environment;
